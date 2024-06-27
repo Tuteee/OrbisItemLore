663351b074be473bca71ce81cb4331d85913f9bb
Inj <108689527+Tuteee@users.noreply.github.com>
2024-06-27 18:34:03 -0400

    Update ItemCraftedListener.java

--- src/main/java/com/orbismc/itemloretowny/listener/ItemCraftedListener.java
+++ src/main/java/com/orbismc/itemloretowny/listener/ItemCraftedListener.java
@@ -1,12 +1,12 @@
 package com.orbismc.itemloretowny.listener;
 
 import com.orbismc.itemloretowny.manager.ItemLoreManager;
-import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.CraftItemEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.Sound;
 
 public class ItemCraftedListener implements Listener {
 
